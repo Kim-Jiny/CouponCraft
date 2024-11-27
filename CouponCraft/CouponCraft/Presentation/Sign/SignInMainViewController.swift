d8562ca3e2015eeb2f2004dab96e90ce361c6763
Jiny <jiny@nachocode.io>
2024-11-27 18:32:15 +0900

    회원가입 UI 구현

--- CouponCraft/CouponCraft/Presentation/Sign/SignInMainViewController.swift
+++ CouponCraft/CouponCraft/Presentation/Sign/SignInMainViewController.swift
@@ -15,13 +15,22 @@ class SignInMainViewController: UIViewController, XibInstantiable {
     
     private var viewModel: SigninViewModel!
     @IBOutlet weak var mainStackView: UIStackView!
+    @IBOutlet weak var signInStackView: UIStackView!
+    @IBOutlet weak var signUpStackView: UIStackView!
     @IBOutlet weak var signInMainView: UIView!
     @IBOutlet weak var textFieldStackView: UIStackView!
+    @IBOutlet weak var signUpTextFieldStackView: UIStackView!
     @IBOutlet weak var enterBtn: UIButton!
     @IBOutlet weak var socialLine: PaddedLabel!
     @IBOutlet weak var socialStackView: UIStackView!
+    @IBOutlet weak var signupSocialStackView: UIStackView!
     @IBOutlet weak var signUpLB: UILabel!
     @IBOutlet weak var signInBottomConstraints: NSLayoutConstraint!
+    @IBOutlet weak var personalBtn: UIButton!
+    @IBOutlet weak var personalImg: UIImageView!
+    @IBOutlet weak var personalLB: UILabel!
+    @IBOutlet weak var signInLB: UILabel!
+    @IBOutlet weak var signUpBtn: UIButton!
     
     // MARK: - Lifecycle
 
@@ -48,7 +57,7 @@ class SignInMainViewController: UIViewController, XibInstantiable {
     
     private func setupViews() {
         textFieldStackView.removeAllArrangedSubviews()
-        mainStackView.setCustomSpacing(30, after: textFieldStackView)
+        signInStackView.setCustomSpacing(30, after: textFieldStackView)
         
         let emailView = UserTextFieldView()
         let emailViewData = UserTextFieldData(title: "Email", placeholder: "your@email.com", type: .email)
@@ -61,29 +70,42 @@ class SignInMainViewController: UIViewController, XibInstantiable {
         textFieldStackView.addArrangedSubview(pwView)
         
         enterBtn.layer.cornerRadius = 20
+        signUpBtn.layer.cornerRadius = 20
         socialLine.padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
         
-        // 전체 문자열
-        let fullText = "Don't have an account? Sign up"
-        let blueBoldText = "Sign up"
-
-        // NSAttributedString 생성
-        let attributedString = NSMutableAttributedString(string: fullText)
-
-        // 특정 텍스트에 색상 및 볼드 폰트 적용
-        if let range = fullText.range(of: blueBoldText) {
-            let nsRange = NSRange(range, in: fullText)
-            let boldFont = UIFont.boldSystemFont(ofSize: 16)
-            attributedString.addAttributes([
-                .foregroundColor: UIColor.speedMain0,
-                .font: boldFont
-            ], range: nsRange)
-        }
-
-        // UILabel에 적용
-        signUpLB.attributedText = attributedString
+        setupAttributedString(fullText: "Don't have an account? Sign up", pointText: "Sign up", lb: signUpLB)
+        signUpLB.isUserInteractionEnabled = true
+        let signUpTapGesture = UITapGestureRecognizer(target: self, action: #selector(signUpTaped))
+        signUpLB.addGestureRecognizer(signUpTapGesture)
+        setupSocialLogin(socialStackView)
         
-        setupSocialLogin()
+        //Sign up
+        signUpStackView.setCustomSpacing(30, after: signUpTextFieldStackView)
+        
+        setupAttributedString(fullText: "I agree to the processing of personal data", pointText: "personal data", lb: personalLB)
+        personalLB.isUserInteractionEnabled = true
+        let personalLBTapGesture = UITapGestureRecognizer(target: self, action: #selector(personalTaped))
+        personalLB.addGestureRecognizer(personalLBTapGesture)
+        personalBtn.addTarget(self, action: #selector(personalCheck), for: .touchUpInside)
+        
+        
+        setupAttributedString(fullText: "Already have an account? Sign in", pointText: "Sign in", lb: signInLB)
+        signInLB.isUserInteractionEnabled = true
+        let signInTapGesture = UITapGestureRecognizer(target: self, action: #selector(signInTaped))
+        signInLB.addGestureRecognizer(signInTapGesture)
+        
+        
+        let signUpEmailView = UserTextFieldView()
+        let signUpEmailViewData = UserTextFieldData(title: "Email", placeholder: "your@email.com", type: .email)
+        signUpEmailView.fill(with: signUpEmailViewData)
+        signUpTextFieldStackView.addArrangedSubview(signUpEmailView)
+        
+        let signUpPwView = UserTextFieldView()
+        let signUpPwViewData = UserTextFieldData(title: "Password", placeholder: "password", type: .password)
+        signUpPwView.fill(with: signUpPwViewData)
+        signUpTextFieldStackView.addArrangedSubview(signUpPwView)
+        
+        setupSocialLogin(signupSocialStackView)
     }
     
     private func bind(to viewModel: SigninViewModel) {
@@ -142,22 +164,22 @@ class SignInMainViewController: UIViewController, XibInstantiable {
         }
     }
     
-    private func setupSocialLogin() {
+    private func setupSocialLogin(_ st: UIStackView) {
         let ggBtn = GIDSignInButton()
         ggBtn.style = .standard
         ggBtn.addTarget(self, action: #selector(googleLogin), for: .touchUpInside)
-        socialStackView.addArrangedSubview(ggBtn)
+        st.addArrangedSubview(ggBtn)
         
         let appleBtn = ASAuthorizationAppleIDButton()
         appleBtn.addTarget(self, action: #selector(appleLogin), for: .touchUpInside)
-        socialStackView.addArrangedSubview(appleBtn)
+        st.addArrangedSubview(appleBtn)
         
         let kakaoBtn = UIButton()
         kakaoBtn.imageView?.contentMode = .scaleAspectFill
         kakaoBtn.imageView?.layer.cornerRadius = 8
         kakaoBtn.setImage(UIImage(named: "kakao_login_medium_wide"), for: .normal)
         kakaoBtn.addTarget(self, action: #selector(kakaoLogin), for: .touchUpInside)
-        socialStackView.addArrangedSubview(kakaoBtn)
+        st.addArrangedSubview(kakaoBtn)
     }
     
     @objc private func googleLogin() {
@@ -213,6 +235,64 @@ class SignInMainViewController: UIViewController, XibInstantiable {
                 }
             }
 //        }
+    }
+    
+    @objc private func signUpTaped() {
+        self.signInStackView.alpha = 0
+        UIView.animate(withDuration: 0.3, animations: {
+            self.signInStackView.isHidden = true
+        }) { _ in
+            self.view.layoutIfNeeded()
+            UIView.animate(withDuration: 0.3, animations: {
+                self.signUpStackView.alpha = 1
+                self.signUpStackView.isHidden = false
+            }) { _ in
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    @objc private func signInTaped() {
+        self.signUpStackView.alpha = 0
+        UIView.animate(withDuration: 0.3, animations: {
+            self.signUpStackView.isHidden = true
+        }) { _ in
+            self.view.layoutIfNeeded()
+            UIView.animate(withDuration: 0.3, animations: {
+                self.signInStackView.alpha = 1
+                self.signInStackView.isHidden = false
+            }) { _ in
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    @objc private func personalTaped() {
+        print("개인정보 처리방침 url 넣고 내부 사파리로 띄워주자")
+    }
+    
+    @objc private func personalCheck() {
+        let checked = self.personalBtn.isSelected
+        self.personalBtn.isSelected = !checked
+        self.personalImg.image = UIImage(systemName: checked ? "checkmark.square.fill" : "checkmark.square")
+    }
+    
+    private func setupAttributedString(fullText: String, pointText: String, lb: UILabel) {
+        // NSAttributedString 생성
+        let attText = NSMutableAttributedString(string: fullText)
+
+        // 특정 텍스트에 색상 및 볼드 폰트 적용
+        if let range = fullText.range(of: pointText) {
+            let nsRange = NSRange(range, in: fullText)
+            let boldFont = UIFont.boldSystemFont(ofSize: 16)
+            attText.addAttributes([
+                .foregroundColor: UIColor.speedMain0,
+                .font: boldFont
+            ], range: nsRange)
+        }
+
+        // UILabel에 적용
+        lb.attributedText = attText
     }
 }
 
